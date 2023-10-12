d92652fc07bee88f759e3244aa75e09b192a1b46
ruchirai <ruchirai175@gmail.com>
2023-10-12 22:51:39 +0530

    store value in database

---
+++ my-app/src/component/index.js
@@ -0,0 +1,28 @@
+// import ReactDOM from "react-dom";
+// import "./index.css";
+// import Header from "./Header";
+// import Footer from "./Footer";
+// import Maincontent from "./Maincontent";
+
+
+// ReactDOM.render(
+//   <>
+//     <Header />
+//     <Maincontent/>
+//     <Footer />
+//   </>,
+//   document.getElementById("root")
+// );
+
+
+
+import React from "react";
+import ReactDOM from "react-dom";
+// import App from "./App";
+// import Promise from "./component/Promise";
+import File from "./component/File";
+
+ReactDOM.render(
+  // <Promise/>,
+   <File/>,
+ document.getElementById("root"));
